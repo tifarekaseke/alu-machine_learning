5ff350d17fee8247821442e2da67aa6480ad990e
t.kaseke@alustudent.com <t.kaseke@alustudent.com>
2026-09-16 13:05:21 +0200

    Add privatized neuron

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Module that defines a neuron for binary classification."""
+
+import numpy as np
+
+
+class Neuron:
+    """Represents a single neuron performing binary classification."""
+
+    def __init__(self, nx):
+        """Initialize a neuron."""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Return the weights vector."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Return the bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Return the activated output."""
+        return self.__A
