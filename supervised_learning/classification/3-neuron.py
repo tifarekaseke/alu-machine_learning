9f61719be9fe790648fa0dc326687136e68522e9
t.kaseke@alustudent.com <t.kaseke@alustudent.com>
2026-09-16 16:32:37 +0200

    Add neuron cost calculation

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Module that defines a neuron for binary classification."""
+
+import numpy as np
+
+
+class Neuron:
+    """Represents a single neuron performing binary classification."""
+
+    def __init__(self, nx):
+        """Initialize a neuron."""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Return the weights vector."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Return the bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Return the activated output."""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculate the forward propagation of the neuron."""
+        z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculate the logistic regression cost."""
+        m = Y.shape[1]
+        cost = -np.sum(
+            Y * np.log(A) +
+            (1 - Y) * np.log(1.0000001 - A)
+        ) / m
+        return cost
